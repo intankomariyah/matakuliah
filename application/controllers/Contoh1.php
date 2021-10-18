a0eed485dc169f177b610837f34221f682d4427f
intankomariyah <intankomariyah16@gmail.com>
2021-10-18 13:30:42 +0700

    Latihan1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Intan Komariyah
+            Saya tinggal di daerah Kedungrandu
+            olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
